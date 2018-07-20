6d1c03863b3743baf8cd37b23a81372b28ed976c
Vince <vincivenv@gmail.com>
2018-07-21 01:20:46 +0300

    Initial Setup

---
+++ lib/movie/list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class list extends StatefulWidget {
+  @override
+  MovieListState createState() {
+    return new MovieListState();
+  }
+}
+
+class MovieListState extends State<list> {
+  @override
+  Widget build(BuildContext context) {}
+}
